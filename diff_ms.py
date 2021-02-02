af5dda81371a4b0f178be59345531b9eac767af2
Leonid Benkevitch <lbenkev@gmail.com>
2021-02-02 17:03:40 -0500

    ...

--- diff_ms.py
+++ diff_ms.py
@@ -47,7 +47,7 @@ ms.open(msn1, nomodify=True)
 ms.selectinit(datadescid=0)      # Reset all previous selections
 
 rec = ms.getdata(cols)
-dat1 = np.squeeze(rec['data'])
+dat1 = np.squeeze(rec['data'])   # Through away zero-length dimensions
 
 ms.close()
 
@@ -59,7 +59,7 @@ ms.open(msn2, nomodify=True)
 ms.selectinit(datadescid=0)      # Reset all previous selections
 
 rec = ms.getdata(cols)
-dat2 = np.squeeze(rec['data'])
+dat2 = np.squeeze(rec['data'])   # Through away zero-length dimensions
 
 ms.close()
 
@@ -68,31 +68,55 @@ ms.close()
 #
 
 #
+# The data in two polarization channels, XX and YY, are equal, so we only
+# leave one: 
+#
+dat1 = dat1[0]
+dat2 = dat2[0]
+
+#
 # Absolute value of the difference are large
 #
 dif = np.abs(dat2 - dat1)      # dif = |dat2 - dat1|
-dif = dif[0]   # The polarizations (?) are the same, so leave only one
 
 #
 # Absolute values before and after SMILI2
 #
-absd1 = np.abs(dat1[0])
-absd2 = np.abs(dat2[0])
-abs_dif_abs = np.abs(absd2 - absd1)
+absd1 = np.abs(dat1)
+absd2 = np.abs(dat2)
+abs_dif_abs_dat12 = np.abs(absd2 - absd1)
 
-print('max(abs_dif_abs) = ', max(abs_dif_abs))
-
-if np.any(abs(absd1 - absd2) == 0.):
-    print('Data in', msn, 'and', msn2, 'are identical.')
-else:
-    print('Data in', msn1, 'and', msn2, 'are different.')
-
-ph = np.angle(dat2 - dat1)[0]
-
-plt.figure(); plt.plot(ph); plt.grid(1) 
+print('max( ||dat2| - |dat1|| ) = %e' % max(abs_dif_abs_dat12))
 
 
+#
+# Plot phases before and after SMILI2 along with their difference
+#
+radeg = 180/np.pi
+ph1 = np.angle(dat1)
+ph2 = np.angle(dat2)
+dif_ph = ph2 - ph1
 
+npt = 130                         # Only plot first npt points
+tim = np.arange(npt)
 
+fig, ax = plt.subplots()           # Create new figure fig with axes ax
 
+ax.plot(dif_ph[:npt]*radeg, 'r', lw=2, label='Phase difference')
+ax.plot(ph1[:npt]*radeg, 'b', label='Phase original')
+ax.plot(ph2[:npt]*radeg, 'g', label='Phase after SMILI2')
+ax.grid(1)
+ax.set_xlim(0,npt)
+ax.set_ylim(-360,360)
+
+ax.set_title('Visibility Phases for ALMA Observations Before and After SMILI2')
+ax.set_xlabel('Time (contingent units)')
+ax.set_ylabel('Phase (deg)')
+degrng = np.arange(-360, 390, 60)
+ax.set_yticklabels(degrng)
+ax.set_yticks(degrng)
+
+ax.legend()
+
+plt.show()
         
